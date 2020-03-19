469c5ced2b3718189f7d7107714c9c4a334be025
Jhonatan <jhonatanrg@live.com>
2020-03-19 17:48:46 -0500

    Major part of the application has been made and prepared. Some errors but compiling ahsdjaks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.styl']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TCG-Angular';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
